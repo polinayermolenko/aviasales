e590e1b069deef93ef5212db9b2bb00a6c889637
polinayermolenko <polina.shvajukova@gmail.com>
2021-01-11 15:50:28 +0300

    Refactoring (store): add redux devtools extension

--- src/store.js
+++ src/store.js
@@ -1,24 +1,9 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
 
 import reducer from './reducers/reducer';
 
-const logger = (store) => (next) => (action) => {
-  console.group(action.type);
-  console.info('dispatching', action);
-  const result = next(action);
-  console.log('next state', store.getState());
-  console.groupEnd(action.type);
-  return result;
-};
-
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
-const enhancer = composeEnhancers(applyMiddleware(ReduxThunk, logger));
-
-const store = createStore(reducer, enhancer);
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(ReduxThunk)));
 
 export default store;
